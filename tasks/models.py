5a108a26f166d45716f8d995f609b06f61d77477
RenatoLemes <renato.lemmes@hotmail.com>
2023-08-05 22:21:33 +0000

    pushing new updated made on tasks

--- tasks/models.py
+++ tasks/models.py
@@ -4,7 +4,7 @@ class Task(models.Model):
   title = models.CharField(max_length = 100)
   due_date = models.DateTimeField()
   overdue = models.BooleanField(default=False)
-  task_owners = models.JSONField(default=list, blank=True)
+  tasks_owners = models.JSONField(default=list, blank=True)
   priority = models.PositiveIntegerField(default=1)
   state = models.CharField(max_length = 15)
   
--- tasks_api/urls.py
+++ tasks_api/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api_auth/', include('rest_framework.urls')),
     path('', include('profiles.urls')),
+    path('', include('tasks.urls')),
 ]
