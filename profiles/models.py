50cb120260a47f128c388b232123ac475e134efc
RenatoLemes <renato.lemmes@hotmail.com>
2023-08-05 16:17:10 +0000

    Added profile serializer and list view

--- profiles/models.py
+++ profiles/models.py
@@ -3,11 +3,11 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 
-class Profile(models.Model):
+class Profile(models.Model): #it need to add something else
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True) #maybe will delete
     name = models.CharField(max_length=255, blank=True)
-    content = models.TextField(blank=True)
+    content = models.TextField(blank=True) #maybe will delete
     image = models.ImageField(
         upload_to='images/', default='../profile_img_z4mi3b'
     )
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Profile
+from .serializers import ProfileSerializer
 
-# Create your views here.
+
+class ProfileList(APIView):
+  def get(self, request):
+    profiles = Profile.objects.all()
+    serializer = ProfileSerializer(profiles, many=True)
+    return Response(serializer.data)
+
+    # def get_queryset(self):
+      
+    # queryset = Task.objects.all()
+    # state = self.request.query_params.get('state', None)
+    # priority = self.request.query_params.get('priority', None)
+
+    # if state is not None:
+    #     queryset = queryset.filter(state=state)
+
+    # if priority is not None:
+    #     queryset = queryset.filter(priority=priority)
+
+    # return queryset
