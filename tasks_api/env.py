b7e4763b3dda535eb7819ac293afa2313e715d7f
RenatoLemes <renato.lemmes@hotmail.com>
2023-08-06 17:21:59 +0000

    test

--- tasks_api/env.py
+++ tasks_api/env.py
@@ -4,3 +4,6 @@ os.environ['CLOUDINARY_URL'] = 'cloudinary://418193753265483:nkgHom4eTX9OomydNiO
 os.environ['CLOUDINARY_CLOUD_NAME'] = 'dyi0up7ld',
 os.environ['CLOUDINARY_API_KEY'] = '418193753265483', 
 os.environ['CLOUDINARY_API_SECRET'] = 'nkgHom4eTX9OomydNiOWYZj2iNA'
+os.environ['DEV'] = 1
+
+os.environ['DATABASE_URL'] = 'postgres://cuynenxr:gDARUhp-AnRNmkN6KOljz940haYRViTo@trumpet.db.elephantsql.com/cuynenxr'
