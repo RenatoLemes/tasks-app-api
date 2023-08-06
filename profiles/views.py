426274785a40c0cf3665b250ed079f6317a4b050
RenatoLemes <renato.lemmes@hotmail.com>
2023-08-06 23:58:22 +0000

    test

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,25 @@
 from django.http import Http404
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .models import Profile
+from .serializers import ProfileSerializer
+from tasks_api.permissions import IsOwnerOrReadOnly
+
+class ProfileList(generics.ListCreateAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+
+
+#????????????>>>>>>>>>>>>>????????????
+
+from django.http import Http404
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -39,6 +60,8 @@ class ProfileDetail(APIView):
           serializer.save()
           return Response(serializer.data)
       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    #??????????????///////////////////////???????????????
 
     # def get_queryset(self):
       
