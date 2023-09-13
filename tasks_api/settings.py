3a4c7fb16c254c8118dd86b1d4289481fb78676c
RenatoLemes <renato.lemmes@hotmail.com>
2023-09-13 16:16:02 +0000

    fixed bug

--- tasks_api/settings.py
+++ tasks_api/settings.py
@@ -62,9 +62,9 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = '12345678' #os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = False
 
-ALLOWED_HOSTS = ['https://8000-renatolemes-tasksappapi-05gokbfsz8f.ws-eu104.gitpod.io/', 'https://task-app-renato-bb143324df86.herokuapp.com']
+ALLOWED_HOSTS = ['8000-renatolemes-tasksappapi-7g01n0ejrm3.ws-eu104.gitpod.io', 'https://task-app-renato-bb143324df86.herokuapp.com']
 
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
